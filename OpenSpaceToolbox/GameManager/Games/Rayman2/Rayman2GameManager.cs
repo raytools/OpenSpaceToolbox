94b030371f6fd79939de74cc97982ce3e2631094
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2025-09-15 23:15:20 +0200

    Fix R2 window detection (part 2)

--- OpenSpaceToolbox/GameManager/Games/Rayman2/Rayman2GameManager.cs
+++ OpenSpaceToolbox/GameManager/Games/Rayman2/Rayman2GameManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
